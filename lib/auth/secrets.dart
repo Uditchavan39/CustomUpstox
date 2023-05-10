ba6b47004475fd8674700cb2ad572c0e1a72f79a
uditchavan <uditchavan39@gmail.com>
2023-05-10 17:07:39 +0530

    commit

--- lib/auth/secrets.dart
+++ lib/auth/secrets.dart
@@ -1,10 +1,9 @@
 class secrets {
-  get apiSecret => 'bi4oly29vo';
-  get apiKey => 'a007dffb-5573-49c5-be2a-3c2539abcdde';
-  get redirectUri => 'https://com.myupstox.256.unique.udituser';
+  get apiSecret => '';
+  get apiKey => '';
+  get redirectUri => '';
 
 //--------------------------------Assumptions made :- change values for different results---------------------
   get yearStart => 22; // collect data from financial year 2020 and after that
-  get yearEnd => 23;    // financial year=yearStart+yearEnd=2021 
-  
+  get yearEnd => 23; // financial year=yearStart+yearEnd=2021
 }
